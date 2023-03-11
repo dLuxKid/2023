b0938b5f3a599efa1eef699a9db82133da442e98
Marvellous Adetunji <adetunjimarvellous09@gmail.com>
2023-03-11 19:50:22 +0100

    starting afresh

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
 import "./App.css";
-import "./reset.css";
-import Navbar from "./components/Navbar/navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import About from "./components/Pages/About";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="pages">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<About />} />
-          {/* <Route
-            path="/github"
-            component={() => {
-              window.location.href = "https://github.com/heisguyy/skinDisease.git";
-              return null;
-            }}
-          /> */}
-        </Routes>
-      </Router>
-    </div>
-  );
+  return <div className=""></div>;
 }
 
 export default App;
