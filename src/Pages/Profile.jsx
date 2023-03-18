2fef62e74470101c6fa2eeb8d687702850d48b4a
Marvellous Adetunji <adetunjimarvellous09@gmail.com>
2023-03-18 19:47:07 +0100

    minor ui changes

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Profile = () => {
-  return <div className="text-center">Loading...</div>;
+  return <div className="text-center bg-[#f5f5f5]">Loading...</div>;
 };
 
 export default Profile;
